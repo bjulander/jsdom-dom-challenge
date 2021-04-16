4442f0e38c473a7e1bd8c8d770ca1187e5248651
Blake Julander <13lakejulander@gmail.com>
2021-04-16 12:09:32 -0700

    commit tree ac747096a445c45399102bc44d1d055433e2e0d8

---
+++ js/challenge.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+    const countVar = document.getElementById("counter")
+    const plusVar = document.getElementById("plus")
+    const minusVar = document.getElementById("minus")
+    const heartVar = document.getElementById("heart")
+    const pauseVar = document.getElementById("pause")
+
+    
+
+
+
+
+
+
+
+});
